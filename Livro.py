d23a0b6d2359eac44a6fbd15b2651eadcf3171c1
AaronMats <202428440@meta.edu.br>
2025-03-21 15:54:42 -0300

    Atualização

--- Livro.py
+++ Livro.py
@@ -1,13 +1,10 @@
 class Livro:
-    def __init__(self, titulo_livro, autor_livro, genero_livro, descricao_livro):
+    def __init__(self, titulo_livro, autor_livro, genero_livro, descricao_livro, edicao):
         self.titulo_livro = titulo_livro
         self.autor_livro = autor_livro
         self.genero_livro = genero_livro
         self.descricao_livro = descricao_livro
-    def detalhes_livro(self):
-        print(f'Título: {self.titulo_livro}\nAutor: {self.autor_livro}\nGenero: {self.genero_livro}\nDescrição: {self.descricao_livro}')
-
-
-
-livro = Livro('duna', 'fulano','sci fi', 'Deserto')
-livro.detalhes_livro()
+        self.edicao = edicao
+    # def detalhes_livro(self):
+    #     print(f'Título: {self.titulo_livro}\nAutor: {self.autor_livro}\nGenero: {self.genero_livro}\nDescrição: {self.descricao_livro}')
+  
---
+++ pessoa.py
@@ -0,0 +1,7 @@
+class Pessoa:
+    def __init__(self, nome, email, cpf, senha):
+        self.nome = nome
+        self.email = email
+        self.cpf = cpf
+        self.senha = senha
+        
