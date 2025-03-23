dcad6f620e4db2a89949cce0ebff7f38fc554634
AaronMats <202428440@meta.edu.br>
2025-03-23 16:51:14 -0300

    Tentando criar uma tela de login

---
+++ screen/Login.py
@@ -0,0 +1,21 @@
+import customtkinter as ctk
+
+class Screen_login():
+
+    ctk.set_appearance_mode('dark')
+    ctk.set_default_color_theme('blue')
+    screen_login = ctk.CTk()
+    screen_login.title('BiblioTec')
+    screen_login.geometry('500x600')
+
+    frame_login = ctk.CTkFrame(screen_login)
+    texto_login = ctk.CTkLabel(frame_login, text="Login")
+    texto_login.pack(padx=10, pady=10)
+    caixa_login_email = ctk.CTkEntry(frame_login, placeholder_text="Digite seu email")
+    caixa_login_email.pack(padx=10, pady= 2)
+    caixa_login_senha = ctk.CTkEntry(frame_login, placeholder_text="Digite sua senha", show="*")
+    caixa_login_senha.pack(padx=10, pady= 2)
+
+    frame_login.pack(fill='both', expand=True)
+
+    screen_login.mainloop()
