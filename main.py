ffb68c40788a1178db9ca4613fd1cb641b076c34
AaronMats <202428440@meta.edu.br>
2025-03-27 18:10:25 -0300

    tete para slavar o adm no json

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from objects.Usuario import Usuario
 from objects.Livro import Livro
 from objects.Pessoa import Pessoa
+from objects.Admin import Admin
+
+# livro1 = Livro("livro1", "Eu", "Ação", "Um livro de ação", "4", 20)
+# livro2 = Livro("livro2", "Tu", "Aventura", "Um livro de aventura", "4", 2)
+# usuario1 = Usuario("fulano", "exemplo@email.com", "123456789000", "123456789")
 
 
-livro1 = Livro("livro1", "Eu", "Ação", "Um livro de ação", "4", 20)
-livro2 = Livro("livro2", "Tu", "Aventura", "Um livro de aventura", "4", 2)
-usuario1 = Usuario("fulano", "exemplo@email.com", "123456789000", "123456789")
 
+# livro1.detalhes()
+# usuario1.alugando(livro1, 4)
+# livro1.detalhes()
+# usuario1.devolve(livro1, 1)
+# livro1.detalhes()
 
+admin_1 = Admin("Fulano", "asdfghjkl", "12345678900", "1234")
 
-livro1.detalhes()
-usuario1.alugando(livro1, 4)
-livro1.detalhes()
-usuario1.devolve(livro1, 1)
-livro1.detalhes()
+novo = admin_1.admin_dic()
+
+admin_1.cadastro_admin(novo)
 
