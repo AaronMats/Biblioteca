3bb7558401af46c1e0db7ffdfbcf05ec4f0fb343
AaronMats <202428440@meta.edu.br>
2025-03-27 16:49:27 -0300

    test 2

--- objects/usuario.py
+++ objects/usuario.py
@@ -47,14 +47,14 @@ class Usuario(Pessoa):
 
 
 
-livro1 = Livro("livro1", "Eu", "Ação", "Um livro de ação", "4", 20)
-livro2 = Livro("livro2", "Tu", "Aventura", "Um livro de aventura", "4", 2)
-usuario1 = Usuario("fulano", "exemplo@email.com", "123456789000", "123456789")
+# livro1 = Livro("livro1", "Eu", "Ação", "Um livro de ação", "4", 20)
+# livro2 = Livro("livro2", "Tu", "Aventura", "Um livro de aventura", "4", 2)
+# usuario1 = Usuario("fulano", "exemplo@email.com", "123456789000", "123456789")
 
 
 
-livro1.detalhes()
-usuario1.alugando(livro1, 4)
-livro1.detalhes()
-usuario1.devolve(livro1, 4)
-livro1.detalhes()
+# livro1.detalhes()
+# usuario1.alugando(livro1, 4)
+# livro1.detalhes()
+# usuario1.devolve(livro1, 4)
+# livro1.detalhes()
